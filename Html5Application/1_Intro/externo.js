bd71e9aa7afa711dedb311d82f7a629fed006cbb
Erick Novais Da Hora <erick.hora@outlook.com>
2019-02-18 07:05:10 -0300

    Add Arquivos

---
+++ Html5Application/1_Intro/externo.js
@@ -0,0 +1,23 @@
+﻿
+window.onload = function () {
+    carrega_nome();
+
+    document.getElementById('um').onclick = function () {
+        Informacao();
+    }
+
+    document.getElementById('dois').ondblclick = function () {
+        Informacao();
+    }
+}
+
+function Informacao() {
+    //boxes - prompt - alert - confirm
+    var nome = window.prompt('Digite o seu nome:');
+    var idade = window.prompt('Digite sua idade:');
+    alert(nome + ' ' + idade);
+}
+
+function carrega_nome() {
+    document.getElementById('nome').value = "Alexandre";
+}
